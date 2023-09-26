34c2d0f4e3af47dc2fba8574336824b8f8dcc3c6
Haroon <haroonnadir143@gmail.com>
2023-09-26 19:27:24 +0500

    first commit

---
+++ project 32 Declare variable using var/script.js
@@ -0,0 +1,7 @@
+
+    var x = 10;
+    if (true) {
+      var x = 20;  // This redeclares the existing 'x' variable.
+      console.log(x);  // Outputs: 20
+    }
+
---
+++ project 33 Declare variable using let/script.js
@@ -0,0 +1,6 @@
+let y = 10;
+  if (true) {
+    let y = 20;  // This is a new variable 'y' within this block.
+    console.log(y);  // Outputs: 20
+  }
+
